3d7ef49277d205cb993f7498d71645405b1c8f30
Michael Opper Moser <waiting4tzeentch@gmail.com>
2016-03-14 18:24:46 -0400

    Added Animal model

---
+++ Cis411_Homework08_Petshop/Models/Animal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Cis411_Homework08_Petshop.Models
+{
+    public class Animal
+    {
+        public Int32 AnimalID { get; set; }
+
+
+        [DisplayName("Animal Species")]
+        [Required()]
+        [StringLength(maximumLength: 20, MinimumLength =3)]
+        public String AnimalSpecies { get; set; }
+
+        [DisplayName("Animal Description")]
+        public String AnimalDescrition { get; set; }
+
+        [Required()]
+        public List<SubSpecies> SubSpecies { get; set; }
+    }
+
+    public class SubSpecies : Animal
+    {
+        public Int32 SpeciesID { get; set; }
+
+        [Required()]
+        [DisplayName("Breed Name")]
+        public String SpeciesName { get; set; }
+
+        [Required()]
+        [DisplayName("Species Description")]
+        public String SpeciesDescription { get; set; }
+    }
+}
---
+++ Cis411_Homework08_Petshop/Models/InventoryDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Cis411_Homework08_Petshop.Models
+{
+    public class InventoryDbContext : DbContext
+    {
+        public DbSet<Animal> Animals { get; set; }
+    }
+}
